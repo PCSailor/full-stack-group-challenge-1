ce54b7518e93303a6df58604b34213044839ba4d
Phil Curtis <Phil@PhilCurtis.io>
2017-03-10 15:53:01 -0600

    base mode warehouse html

--- server/public/scripts/controllers/warehouse.controller.js
+++ server/public/scripts/controllers/warehouse.controller.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 App.controller('WarehouseController', ['$http', 'WarehouseFactory', function($http, WarehouseFactory){
-=======
-App.controller('warehousecontroller', ['$http', 'WarehouseFactory', function($http, WarehouseFactory){
->>>>>>> ca43a331827fa780a3c071e874b2d493aac613de
 console.log('warehousecontroller loaded');
 var self = this;
 self.newWarehouse = {};
